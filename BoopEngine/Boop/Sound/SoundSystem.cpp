81014391c7ada60fbc24db7277095dfd909a28a2
Bauwke <Bauwke@BOOPPC>
2025-04-27 10:41:49 +0200

    add SDL_mixer 2 in cmake

--- BoopEngine/Boop/Sound/SoundSystem.cpp
+++ BoopEngine/Boop/Sound/SoundSystem.cpp
@@ -3,14 +3,14 @@
 #include "ServiceLocator.h"
 #include <iostream>
 
-void boop::SoundSystemNull::Play(const unsigned short id, const float volume)
+void boop::SoundSystemNull::PlaySound(const unsigned short id, const float volume)
 {
 	// code that actually plays sound here
 }
 
 std::unique_ptr<boop::SoundSystem> boop::ServiceLocator::m_SoundSystemInstance{ std::make_unique<SoundSystemNull>() };
 
-void boop::SoundSystemLogger::Play(const unsigned short id, const float volume)
+void boop::SoundSystemLogger::PlaySound(const unsigned short id, const float volume)
 {
 	//Log the sound play action
 		std::cout << "Playing sound with ID: " << id << " at volume: " << volume << std::endl;
--- BoopEngine/Boop/Sound/SoundSystem.h
+++ BoopEngine/Boop/Sound/SoundSystem.h
@@ -6,20 +6,20 @@ namespace boop
 	{
 	public:
 		virtual ~SoundSystem() = default;
-		virtual void Play(const unsigned short id, const float volume) = 0;
+		virtual void PlaySound(const unsigned short id, const float volume) = 0;
 	};
 
 
 	class SoundSystemNull final : public SoundSystem
 	{
 	public:
-		void Play(const unsigned short id, const float volume) override;
+		void PlaySound(const unsigned short id, const float volume) override;
 	};
 
 
 	class SoundSystemLogger final : public SoundSystem
 	{
 	public:
-		void Play(const unsigned short id, const float volume) override;
+		void PlaySound(const unsigned short id, const float volume) override;
 	};
 }
