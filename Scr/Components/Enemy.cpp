add51fb798c070c1724580d6792a2ae7066bbdd1
Bauwke <Bauwke@BOOPPC>
2025-06-05 23:41:34 +0200

    fix: enemy can spot player in FOV

--- Scr/Components/Enemy.cpp
+++ Scr/Components/Enemy.cpp
@@ -45,55 +45,49 @@ glm::vec2 Enemy::SeePlayer()
 
 	glm::vec2 resultPos{0,0};
 
-	if (player1 && player2)
-	{
-		auto tankPos = m_pTankBase->GetOwner()->GetWorldPosition();
+	//if (player1 && player2)
+	//{
+	//	auto tankPos = m_pTankBase->GetOwner()->GetWorldPosition();
 
-		//check player1
-		resultPos = CheckPlayerPosSeen(player1->GetWorldPosition());
-		//check player2
-		if (resultPos == glm::vec2{0,0}) resultPos = CheckPlayerPosSeen(player2->GetWorldPosition());
+	//	//check player1
+	//	resultPos = CheckPlayerPosSeen(player1->GetWorldPosition());
+	//	//check player2
+	//	if (resultPos == glm::vec2{0,0}) resultPos = CheckPlayerPosSeen(player2->GetWorldPosition());
 
-		////check player 1
-		//auto player1Pos = player1->GetWorldPosition();
+	//}
+	//else {
+	//	//no second player, just go to player 1
+	//	float distanceToPlayer1 = glm::distance(m_pTankBase->GetOwner()->GetWorldPosition(), player1->GetWorldPosition());
 
-		////x check
-		//if (player1Pos.x - 15 < tankPos.x && player1Pos.x + 15 > tankPos.x) return player1Pos;
-		////y check
-		//if (player1Pos.y - 15 < tankPos.y && player1Pos.y + 15 > tankPos.y) return player1Pos;
+	//	if (distanceToPlayer1 < 100.f)
+	//	{
+	//		std::cout << "PLayer1x: " << roundf(player1->GetWorldPosition().x) << " Player1y: " << roundf(player1->GetWorldPosition().y) << std::endl;
+	//		std::cout << "enemyx: " << m_pTankBase->GetOwner()->GetWorldPosition().x << " enemyy: " << m_pTankBase->GetOwner()->GetWorldPosition().y << std::endl;
+
+	//	}
+	//}
+
+	//check player1
+	resultPos = CheckPlayerPosSeen(player1->GetWorldPosition());
+	//check player2
+	if (player2 && resultPos == glm::vec2{ 0,0 }) resultPos = CheckPlayerPosSeen(player2->GetWorldPosition());
+
+	//debug check
+	if (resultPos != glm::vec2{ 0,0 }) std::cout << "PLAYER SEEN!\n";
 
 
-		////check player 2
-		//auto player2Pos = player2->GetWorldPosition();
-		////x check
-		//if (player2Pos.x - 15 < tankPos.x && player2Pos.x + 15 > tankPos.x) return player2Pos;
-		////y check
-		//if (player2Pos.y - 15 < tankPos.y && player2Pos.y + 15 > tankPos.y) return player2Pos;
-
-	}
-	else {
-		//no second player, just go to player 1
-		float distanceToPlayer1 = glm::distance(m_pTankBase->GetOwner()->GetWorldPosition(), player1->GetWorldPosition());
-
-		if (distanceToPlayer1 < 100.f)
-		{
-			std::cout << "PLayer1x: " << roundf(player1->GetWorldPosition().x) << " Player1y: " << roundf(player1->GetWorldPosition().y) << std::endl;
-			std::cout << "enemyx: " << m_pTankBase->GetOwner()->GetWorldPosition().x << " enemyy: " << m_pTankBase->GetOwner()->GetWorldPosition().y << std::endl;
-
-		}
-	}
-
-	return glm::vec2{0,0};
+	return resultPos;
 }
 
 glm::vec2 Enemy::CheckPlayerPosSeen(glm::vec2 playerPos)
 {
 	auto tankPos = m_pTankBase->GetOwner()->GetWorldPosition();
+	int margin = 15;
 
 	//x check
-	if (playerPos.x - 15 < tankPos.x && playerPos.x + 15 > tankPos.x) return playerPos;
+	if (playerPos.x - margin < tankPos.x && playerPos.x + margin > tankPos.x) return playerPos;
 	//y check
-	if (playerPos.y - 15 < tankPos.y && playerPos.y + 15 > tankPos.y) return playerPos;
+	if (playerPos.y - margin < tankPos.y && playerPos.y + margin > tankPos.y) return playerPos;
 
 	return glm::vec2{ 0,0 }; //not seen
 }
