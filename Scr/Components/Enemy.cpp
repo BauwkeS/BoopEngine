b86d445e9ad7490a0d96c5005dead4f2ae8227a6
Bauwke <Bauwke@BOOPPC>
2025-06-05 23:41:56 +0200

    cleanup comments

--- Scr/Components/Enemy.cpp
+++ Scr/Components/Enemy.cpp
@@ -45,36 +45,13 @@ glm::vec2 Enemy::SeePlayer()
 
 	glm::vec2 resultPos{0,0};
 
-	//if (player1 && player2)
-	//{
-	//	auto tankPos = m_pTankBase->GetOwner()->GetWorldPosition();
-
-	//	//check player1
-	//	resultPos = CheckPlayerPosSeen(player1->GetWorldPosition());
-	//	//check player2
-	//	if (resultPos == glm::vec2{0,0}) resultPos = CheckPlayerPosSeen(player2->GetWorldPosition());
-
-	//}
-	//else {
-	//	//no second player, just go to player 1
-	//	float distanceToPlayer1 = glm::distance(m_pTankBase->GetOwner()->GetWorldPosition(), player1->GetWorldPosition());
-
-	//	if (distanceToPlayer1 < 100.f)
-	//	{
-	//		std::cout << "PLayer1x: " << roundf(player1->GetWorldPosition().x) << " Player1y: " << roundf(player1->GetWorldPosition().y) << std::endl;
-	//		std::cout << "enemyx: " << m_pTankBase->GetOwner()->GetWorldPosition().x << " enemyy: " << m_pTankBase->GetOwner()->GetWorldPosition().y << std::endl;
-
-	//	}
-	//}
-
 	//check player1
 	resultPos = CheckPlayerPosSeen(player1->GetWorldPosition());
 	//check player2
 	if (player2 && resultPos == glm::vec2{ 0,0 }) resultPos = CheckPlayerPosSeen(player2->GetWorldPosition());
 
 	//debug check
-	if (resultPos != glm::vec2{ 0,0 }) std::cout << "PLAYER SEEN!\n";
-
+	//if (resultPos != glm::vec2{ 0,0 }) std::cout << "PLAYER SEEN!\n";
 
 	return resultPos;
 }
