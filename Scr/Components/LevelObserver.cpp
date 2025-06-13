19c7de37e95b88d2653c53542cafd73189b7f3f7
Bauwke <Bauwke@BOOPPC>
2025-06-13 22:48:12 +0200

    fix: correct names on leaderboard input

--- Scr/Components/LevelObserver.cpp
+++ Scr/Components/LevelObserver.cpp
@@ -1,3 +1,4 @@
+#pragma once
 #include "LevelObserver.h"
 #include "../../BoopEngine/Boop/GameObject.h"
 #include "../../BoopEngine/Boop/Scene/SceneManager.h"
@@ -14,9 +15,12 @@ void LevelObserver::OnNotify(boop::Event event)
 	//GO TO END SCREEN
 	if (event.id == boop::make_sdbm_hash("PlayerDied"))
 	{
-		SetUpEndScreen();
+		boop::InputManager::GetInstance().ClearCommands();
+		SetEndData();
 
-		auto changeScene = booble::ChangeScene("EndingScreen");
+		MakeInputEndScreen();
+
+		auto changeScene = booble::ChangeScene("InputEndingScreen");
 		changeScene.Execute();
 	}
 
@@ -52,13 +56,12 @@ void LevelObserver::SetUpEndScreen()
 	const std::string levelName{ "EndingScreen" };
 	auto& endingScene = boop::SceneManager::GetInstance().AddScene(levelName);
 
-	Highscores::highscoreData endData = GetEndData();
-	m_Highscores->AddHighScore(endData);
+	m_Highscores->AddHighScore(m_EndData);
 
 	//selection info
 	auto mainTexts = std::make_unique<boop::GameObject>();
-	mainTexts->AddComponent<boop::TextComponent>("GAME OVER: " + endData.playerWon + " won!")->SetPosition(300, 100);
-	mainTexts->AddComponent<boop::TextComponent>("With score: " + std::to_string(endData.score))->SetPosition(300, 200);
+	mainTexts->AddComponent<boop::TextComponent>("GAME OVER: " + m_EndData.playerWon + " won!")->SetPosition(300, 100);
+	mainTexts->AddComponent<boop::TextComponent>("With score: " + std::to_string(m_EndData.score))->SetPosition(300, 200);
 
 	endingScene.Add(std::move(mainTexts));
 
@@ -77,10 +80,10 @@ void LevelObserver::SetUpEndScreen()
 
 	endingScene.Add(std::move(scoretext));
 }
-Highscores::highscoreData LevelObserver::GetEndData()
+void LevelObserver::SetWinningScore()
 {
-	int endScore{0};
-	std::string playerWon{"Player 1"};
+	//--SCORE
+	int endScore{ 0 };
 
 	//delete the players if they exist
 	auto scene = boop::SceneManager::GetInstance().GetActiveScene();
@@ -90,45 +93,112 @@ Highscores::highscoreData LevelObserver::GetEndData()
 		player1->SetToDelete();
 	}
 
-	switch (static_cast<booble::GameMode>(booble::GameLoader::GetInstance().GetSelectedGamemode()))
-	{
-		case booble::GameMode::MULTIPLAYER:
-		{
-			//get player 2 and delete it
-			auto player2 = scene->FindGameObjectByTag("p2");
-			if (!player2) break;
+	//get player 2 and delete it
+	auto player2 = scene->FindGameObjectByTag("p2");
+	if (player2) {
+		//get score from player 2
+		int scorePlayer2 = player2->GetComponent<booble::Player>()->GetScore();
 
-			//get score from player 2
-			int scorePlayer2 = player2->GetComponent<booble::Player>()->GetScore();
-
-			//this is versus so check which player won and that will be the end score
-			if (endScore < scorePlayer2) {
-				endScore = scorePlayer2;
-				playerWon = "Player 2"; //player 2 won
-			}
-			player2->SetToDelete();
-
-			break;
+		//this is versus so check which player won and that will be the end score
+		if (endScore < scorePlayer2) {
+			endScore = scorePlayer2;
 		}
-		case booble::GameMode::COOP:
-		{
-			playerWon = "Both players";
-
-			//get player 2 and delete it
-			auto player2 = scene->FindGameObjectByTag("p2");
-			if (!player2) break;
-			
-
-			//scores combine /2 to make the leaderboard fair
-
-			endScore += player2->GetComponent<booble::Player>()->GetScore();
-			endScore /= 2;
-
-			player2->SetToDelete();
-
-			break;
-		}
+		player2->SetToDelete();
 	}
 
-	return Highscores::highscoreData{playerWon, endScore};
+
+	m_EndData.score = endScore;
+}
+void LevelObserver::SetEndData()
+{
+	SetWinningScore();
+	SetWinningPlayer();
+}
+void LevelObserver::SetWinningPlayer()
+{
+	//--PLAYER
+	//get player from the 3 input letters
+	std::string playerWon;
+	for (int i = 0; i < 3; ++i)
+	{
+		playerWon += std::string(1,m_Letters[m_LetterIndexes[i]]);
+	}
+
+	m_EndData.playerWon = playerWon;
+}
+
+
+void LevelObserver::MakeInputEndScreen()
+{
+	//just the player input here to make the name
+
+	//SETUP
+	const std::string levelName{ "InputEndingScreen" };
+	auto& endingScene = boop::SceneManager::GetInstance().AddScene(levelName);
+
+	std::string letter1(1, m_Letters[m_LetterIndexes[0]]);
+	std::string letter2(1, m_Letters[m_LetterIndexes[1]]);
+	std::string letter3(1, m_Letters[m_LetterIndexes[2]]);
+
+	//selection info
+	auto mainTexts = std::make_unique<boop::GameObject>();
+	mainTexts->AddComponent<boop::TextComponent>("Select Name: ")->SetPosition(300, 100);
+	auto slectionText = std::make_unique<boop::GameObject>();
+	slectionText->AddComponent<boop::TextComponent>(letter1)->SetPosition(300, 170);
+	slectionText->AddComponent<boop::TextComponent>(letter2)->SetPosition(330, 170);
+	slectionText->AddComponent<boop::TextComponent>(letter3)->SetPosition(360, 170);
+	slectionText->SetTag("selection");
+	mainTexts->AddComponent<boop::TextComponent>("Use arrow keys/buttons:")->SetPosition(200, 300);
+	mainTexts->AddComponent<boop::TextComponent>("- up/down to change letter")->SetPosition(200, 330);
+	mainTexts->AddComponent<boop::TextComponent>("- left right to switch letter ")->SetPosition(200, 360);
+	mainTexts->AddComponent<boop::TextComponent>("Press enter to continue")->SetPosition(200, 400);
+
+	//---
+	//INPUT
+
+	boop::InputManager::GetInstance().AddCommand(SDL_SCANCODE_RETURN, boop::keyState::isDown,
+		std::make_unique<GoToEndScreen>(this));
+
+	boop::InputManager::GetInstance().AddCommand(SDL_SCANCODE_UP, boop::keyState::isDown,
+		std::make_unique<ChangeLetter>(this, m_LetterIndexes, m_SelectedLetter, true));
+	boop::InputManager::GetInstance().AddCommand(SDL_SCANCODE_DOWN, boop::keyState::isDown,
+		std::make_unique<ChangeLetter>(this, m_LetterIndexes, m_SelectedLetter, false));
+	boop::InputManager::GetInstance().AddCommand(SDL_SCANCODE_RIGHT, boop::keyState::isDown,
+		std::make_unique<ChangeSelection>(m_SelectedLetter, true));
+	boop::InputManager::GetInstance().AddCommand(SDL_SCANCODE_LEFT, boop::keyState::isDown,
+		std::make_unique<ChangeSelection>(m_SelectedLetter, false));
+
+
+	endingScene.Add(std::move(slectionText));
+	endingScene.Add(std::move(mainTexts));
+}
+
+
+//-------------------
+void ChangeLetter::Execute()
+{
+	// Update the text
+	if (!m_MoveUp) m_Letters[m_SelectedLetter] = (m_Letters[m_SelectedLetter] + 1) % 26; // Wrap around after 'Z'
+	else m_Letters[m_SelectedLetter] = (m_Letters[m_SelectedLetter] - 1 + 26) % 26; // Wrap around before 'A'
+
+	auto letters = m_pGameObject->GetLetters();
+
+	auto textGO = boop::SceneManager::GetInstance().GetActiveScene()->FindGameObjectByTag("selection");
+	auto textsC = textGO->GetSomeComponent<boop::TextComponent>();
+
+	textsC[m_SelectedLetter]->SetText(std::string(1, m_pGameObject->GetLetters()[m_Letters[m_SelectedLetter]]));
+}
+
+void ChangeSelection::Execute()
+{
+	if (m_MoveUp) m_Letterindex = (m_Letterindex + 1) % 3; 
+	else m_Letterindex = (m_Letterindex - 1 + 3) % 3;
+}
+
+void GoToEndScreen::Execute()
+{
+	m_pLevelObs->SetWinningPlayer();
+	m_pLevelObs->SetUpEndScreen();
+	booble::ChangeScene changeScene("EndingScreen");
+	changeScene.Execute();
 }
--- Scr/Components/LevelObserver.h
+++ Scr/Components/LevelObserver.h
@@ -4,7 +4,9 @@
 #include "../../BoopEngine/Boop/Event/Event.h"
 #include <string>
 #include "../Highscores.h"
-
+#include "../../BoopEngine/Boop/Input/Command.h"
+#include "../../BoopEngine/Boop/Components/TextComponent.h"
+#include <span>
 
 class boop::GameObject;
 
@@ -16,9 +18,86 @@ public:
 
 	void OnNotify(boop::Event event) override;
 
-private:
+	//const char(&GetLetters() const)[27] {return m_Letters;}
+
+	std::span<const char> GetLetters() const { return m_Letters; }
+	int GetSelectedLetter() const { return m_SelectedLetter; }
+
 	void SetUpEndScreen();
-	Highscores::highscoreData GetEndData();
+	void SetWinningPlayer();
+private:
+	void SetEndData();
+	void SetWinningScore();
 
 	std::unique_ptr<Highscores> m_Highscores{};
+	Highscores::highscoreData m_EndData{};
+
+	//Input stuff
+	void MakeInputEndScreen();
+
+	char m_Letters[27] = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+		'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+
+	int m_LetterIndexes[3]{0,0,0};
+	int m_SelectedLetter{ 0 };
+};
+
+class ChangeLetter final : public boop::Command {
+private:
+	LevelObserver* m_pGameObject;
+	int* m_Letters;
+	int& m_SelectedLetter;
+	bool m_MoveUp;
+public:
+	ChangeLetter(LevelObserver* component, int* letters, int& letter, bool moveUp)
+		: m_pGameObject{ component }, m_Letters{ letters }, m_SelectedLetter{
+		letter
+	}, m_MoveUp{ moveUp } {
+	}
+	~ChangeLetter() { m_pGameObject = nullptr;}
+
+	void Execute() override;
+
+	ChangeLetter(const ChangeLetter& other) = delete;
+	ChangeLetter(ChangeLetter&& other) = delete;
+	ChangeLetter& operator=(const ChangeLetter& other) = delete;
+	ChangeLetter& operator=(ChangeLetter&& other) = delete;
+
+};
+
+class ChangeSelection final : public boop::Command {
+private:
+	int& m_Letterindex;
+	bool m_MoveUp;
+public:
+	ChangeSelection(int& letter, bool moveUp)
+		: m_Letterindex{ letter }, m_MoveUp{ moveUp } {
+	}
+	~ChangeSelection() { }
+
+	void Execute() override;
+
+	ChangeSelection(const ChangeSelection& other) = delete;
+	ChangeSelection(ChangeSelection&& other) = delete;
+	ChangeSelection& operator=(const ChangeSelection& other) = delete;
+	ChangeSelection& operator=(ChangeSelection&& other) = delete;
+
+};
+
+
+class GoToEndScreen final : public boop::Command {
+private:
+	LevelObserver* m_pLevelObs;
+public:
+	GoToEndScreen(LevelObserver* LevelObs) : m_pLevelObs{LevelObs} {
+	}
+	~GoToEndScreen() {}
+
+	void Execute() override;
+
+	GoToEndScreen(const GoToEndScreen& other) = delete;
+	GoToEndScreen(GoToEndScreen&& other) = delete;
+	GoToEndScreen& operator=(const GoToEndScreen& other) = delete;
+	GoToEndScreen& operator=(GoToEndScreen&& other) = delete;
+
 };
